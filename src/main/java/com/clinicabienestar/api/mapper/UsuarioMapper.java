19f3bdabb0b0915570cf107bbad6e459e72a82c4
JonathanRAD <jonathanrujel4@gmail.com>
2025-10-14 15:22:20 -0500

    feat: revisión final de refactorizacion-backend

--- src/main/java/com/clinicabienestar/api/mapper/UsuarioMapper.java
+++ src/main/java/com/clinicabienestar/api/mapper/UsuarioMapper.java
@@ -3,6 +3,7 @@ package com.clinicabienestar.api.mapper;
 import com.clinicabienestar.api.dto.UsuarioDTO;
 import com.clinicabienestar.api.model.Usuario;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -12,8 +13,14 @@ public interface UsuarioMapper {
 
     UsuarioMapper INSTANCE = Mappers.getMapper(UsuarioMapper.class);
 
+    @Mapping(target = "fechaRegistro", ignore = true)
     UsuarioDTO toDTO(Usuario usuario);
 
+    // --- CORRECCIÓN ---
+    // Eliminamos la línea de "authorities"
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "intentosFallidos", ignore = true)
+    @Mapping(target = "bloqueoExpiracion", ignore = true)
     Usuario toEntity(UsuarioDTO usuarioDTO);
 
     List<UsuarioDTO> toDTOList(List<Usuario> usuarios);
