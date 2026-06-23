403050ff641dad537aec0f8114da9ce98a621766
JonathanRAD <jonathanrujel4@gmail.com>
2026-06-23 15:05:05 -0500

    fix: zona horaria

--- src/main/java/com/clinicabienestar/api/ApiApplication.java
+++ src/main/java/com/clinicabienestar/api/ApiApplication.java
@@ -2,9 +2,16 @@ package com.clinicabienestar.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class ApiApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
